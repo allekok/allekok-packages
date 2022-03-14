1ddbd0c81f4347a88ca3072d88a18697a2ada68f
Payam <payambapiri.97@gmail.com>
2022-03-14 19:30:11 +0330

    allekok-colorful-theme/code.js: Update code

--- allekok-colorful-theme/code.js
+++ allekok-colorful-theme/code.js
@@ -2,24 +2,24 @@
 	const poets = document.querySelectorAll('.poet')
 	const style = document.createElement('STYLE')
 	let poets_len = poets.length ? poets.length : 200
-	let s = `.poet { ` +
-	    `border-radius:.2rem;` +
-	    `max-width:100px;` +
-	    `margin:1%;` +
-	    `width:23%` +
-	    ` } ` +
-	    `.poet:hover { ` +
-	    `opacity:.8` +
-	    ` } ` +
-	    `.poet img { ` +
-	    `border-radius:.2rem .2rem 0 0;` +
-	    `width:100%;` +
-	    `background:#EEE` +
-	    ` } ` +
-	    `.poet h3 { ` +
-	    `color:#000;` +
-	    `text-shadow:1px 0 1px #FFF` +
-	    ` } `
+	let s = '.poet { ' +
+	    'border-radius:.2rem;' +
+	    'max-width:100px;' +
+	    'margin:1%;' +
+	    'width:23%' +
+	    ' } ' +
+	    '.poet:hover { ' +
+	    'opacity:.8' +
+	    ' } ' +
+	    '.poet img { ' +
+	    'border-radius:.2rem .2rem 0 0;' +
+	    'width:100%;' +
+	    'background:#EEE' +
+	    ' } ' +
+	    '.poet h3 { ' +
+	    'color:#000;' +
+	    'text-shadow:1px 0 1px #FFF' +
+	    ' } '
 
 	for( ; poets_len; poets_len--) {
 		s += (`.poet:nth-child(${poets_len}) { ` +
@@ -31,10 +31,9 @@
 	document.head.appendChild(style)
 
 	function random_color() {
-		const colors = ['#FFCBCB', '#E7FBBE', '#FFFDDE',
-				'#D9D7F1', '#FFBED8', '#EDD2F3',
-				'#FEE3EC',  '#F9C5D5', '#FFFEA9',
-				'#D7E9F7']
+		const colors = ['#FFCBCB', '#E7FBBE', '#FFFDDE', '#D9D7F1',
+				'#FFBED8', '#EDD2F3', '#FEE3EC', '#F9C5D5',
+				'#FFFEA9', '#D7E9F7']
 		return colors[Math.floor(Math.random() * colors.length)]
 	}
 })()
