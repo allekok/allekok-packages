be53f89fd90d2d96efb470e05fe97447a12e5f6b
Payam <payambapiri.97@gmail.com>
2022-03-14 18:52:14 +0330

    allekok-colorful-theme/code.js: Fix an issue that happened when there is no ".poet" element

--- allekok-colorful-theme/code.js
+++ allekok-colorful-theme/code.js
@@ -1,25 +1,29 @@
 (function allekok_colorful_theme() {
 	const poets = document.querySelectorAll('.poet')
 	const style = document.createElement('STYLE')
+	let poets_len = poets.length ? poets.length : 200
 	let s = `.poet { ` +
 	    `border-radius:.2rem;` +
 	    `max-width:100px;` +
 	    `margin:1%;` +
 	    `width:23%` +
 	    ` } ` +
+	    `.poet:hover { ` +
+	    `opacity:.8` +
+	    ` } ` +
 	    `.poet img { ` +
 	    `border-radius:.2rem .2rem 0 0;width:100%;background:#EEE` +
 	    ` } ` +
 	    `.poet h3 { ` +
+	    `color:#000;` +
 	    `text-shadow:1px 0 1px #FFF` +
 	    ` } `
-	let i = 1
 
-	poets.forEach(p => {
-		s += (`.poet:nth-child(${i++}) { ` +
+	for( ; poets_len; poets_len--) {
+		s += (`.poet:nth-child(${poets_len}) { ` +
 		      `background:${random_color()}` +
 		      ` } `)
-	})
+	}
 
 	style.innerHTML = s
 	document.head.appendChild(style)
