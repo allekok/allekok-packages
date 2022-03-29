4f127e1d0687a94d0080d45c5437da0be18c5c52
Payam <payambapiri.97@gmail.com>
2022-03-29 19:22:12 +0430

    iframe: New package added

---
+++ iframe/code.js
@@ -0,0 +1,43 @@
+(function iframe() {
+	const url = 'https://books.vejin.net/'
+	const iframe = document.createElement('IFRAME')
+	const iframe_style = ('width:48%;height:100%;display:inline-block;' +
+			      'box-sizing:border-box;border:1px solid;' +
+			      'vertical-align:top;margin-left:2%;' +
+			      'border-radius:1rem;')
+	const main_id = 'MAIN'
+	const main_style = 'width:50%;display:inline-block'
+
+	let old_ajax_load
+
+	window.addEventListener('load', () => {
+		const main = document.getElementById(main_id)
+		
+		old_ajax_load = ajax_load
+		ajax_load = new_ajax_load
+		
+		iframe.src = url
+		iframe.style = iframe_style
+		document.body.insertBefore(iframe, main)
+
+		set_main_style(main)
+		resize()
+	})
+	
+	function new_ajax_load(url, href, content, parent, target, loading) {
+		old_ajax_load(url, href, content, parent, target, loading)
+		const main = document.getElementById(main_id)
+		set_main_style(main)
+	}
+
+	function resize(interval=1000) {
+		return setInterval(() => {
+			const main = document.getElementById(main_id)
+			iframe.style.height = `${main.offsetHeight}px`
+		}, interval)
+	}
+
+	function set_main_style(main) {
+		main.style = main_style
+	}
+}());
