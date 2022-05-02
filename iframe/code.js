51513597927f85e97e1568b1a62ad1a0ab45093a
Payam <payambapiri.97@gmail.com>
2022-05-02 21:47:52 +0430

    iframe/code.js: Remove extra spaces

--- iframe/code.js
+++ iframe/code.js
@@ -12,10 +12,10 @@
 
 	window.addEventListener('load', () => {
 		const main = document.getElementById(main_id)
-		
+
 		old_ajax_load = ajax_load
 		ajax_load = new_ajax_load
-		
+
 		iframe.src = url
 		iframe.style = iframe_style
 		document.body.insertBefore(iframe, main)
@@ -23,20 +23,18 @@
 		set_main_style(main)
 		resize()
 	})
-	
+
 	function new_ajax_load(url, href, content, parent, target, loading) {
 		old_ajax_load(url, href, content, parent, target, loading)
 		const main = document.getElementById(main_id)
 		set_main_style(main)
 	}
-
 	function resize(interval=1000) {
 		return setInterval(() => {
 			const main = document.getElementById(main_id)
 			iframe.style.height = `${main.offsetHeight}px`
 		}, interval)
 	}
-
 	function set_main_style(main) {
 		main.style = main_style
 	}
